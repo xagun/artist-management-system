623309a5e71a81cab759ec72ddfc941544bb0972
Sagun <sagun@SagunShrestha.local>
2024-05-20 11:44:34 +0545

    create migrations for users artists and music table

---
+++ database/migrations/2024_05_20_053601_create_artists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamp('dob');
+            $table->enum('gender', ['m', 'f', 'o']);
+            $table->string('address')->nullable();
+            $table->year('first_release_year');
+            $table->integer('no_of_albums_released')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('artists');
+    }
+};
