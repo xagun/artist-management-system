f304dd7f935143dca37e4a87a98536fd217557aa
xagun <xagun.shrestha@gmail.com>
2024-05-22 16:33:27 +0545

    feat: create login page ui

--- AMS-frontend/src/layout/DefaultLayout.tsx
+++ AMS-frontend/src/layout/DefaultLayout.tsx
@@ -1,20 +1,49 @@
 import { useStateContext } from "@/context/ContextProvider";
-import { Navigate, Outlet } from "react-router-dom";
-
+import { Link, Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function DefaultLayout() {
-
-    const {token} = useStateContext();
-
-    if(token){
-        return <Navigate to = "/dashboard" />
+    const { token } = useStateContext();
+    const location = useLocation();
+    const pathName = location.pathname;
+    if (token) {
+        return <Navigate to="/dashboard" />;
     }
 
-  return (
-    <div>
-        Welcome to the Artist Management System
-        {/* This Outlet is used to render child route elements here which is defined in routes.tsx*/}
-        <Outlet />
-    </div>
-  )
+    return (
+        <div className="flex items-center min-h-screen p-4 bg-gray-100 lg:justify-center font-lato ">
+            <div className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-[800px]">
+                <div className="p-4 py-6 text-white bg-blue-500 md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly">
+                    <div className="my-3 text-4xl font-bold tracking-wider text-center">
+                        <span>AMS</span>
+                        <div className="text-xs">Artist Management System</div>
+                    </div>
+                    <p className="mt-6 font-normal text-center md:mt-0 text-sm ">
+                        Login or sign up now to experience the future of artist
+                        management. With our platform, you can not only view
+                        artists and their music but also take control of your
+                        music universe.
+                    </p>
+                    <p className="flex flex-col items-center justify-center mt-10 text-center">
+                        <span>
+                            {pathName.match("login")
+                                ? "Don't have an account?"
+                                : "Already have an account"}
+                        </span>
+                        {pathName.match("login") ? (
+                            <Link to="/register" className="font-bold">
+                                Register Here
+                            </Link>
+                        ) : (
+                            <Link to="/login" className="font-bold">
+                                Login Here
+                            </Link>
+                        )}
+                    </p>
+                </div>
+                <div className="p-5 bg-white md:flex-1">
+                    <Outlet />
+                </div>
+            </div>
+        </div>
+    );
 }
--- AMS-frontend/src/views/Login.tsx
+++ AMS-frontend/src/views/Login.tsx
@@ -1,10 +1,59 @@
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function Login() {
-  return (
-    <div>Login
+    const [email, setEmail] = useState<string>("");
+    const [passowrd, setPassowrd] = useState<string>("");
 
-        <Button>Hello Login</Button>
-    </div>
-  )
+    const handleLogin = () => {};
+
+    return (
+        <div>
+            <h3 className="my-4 text-2xl font-semibold text-gray-700">
+                Account Login
+            </h3>
+            <div className="flex flex-col space-y-5">
+                <div className="flex flex-col space-y-1">
+                    <label
+                        htmlFor="email"
+                        className="text-sm font-semibold text-gray-500"
+                    >
+                        Email address
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        autoFocus
+                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                    />
+                </div>
+                <div className="flex flex-col space-y-1">
+                    <div className="flex items-center justify-between">
+                        <label
+                            htmlFor="password"
+                            className="text-sm font-semibold text-gray-500"
+                        >
+                            Password
+                        </label>
+                    </div>
+                    <input
+                        type="password"
+                        id="password"
+                        value={passowrd}
+                        onChange={(e) => setPassowrd(e.target.value)}
+                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
+                    />
+                </div>
+
+                <Button
+                    className="w-[140px] px-4 py-2 text-sm font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+                    onClick={handleLogin}
+                >
+                    Login
+                </Button>
+            </div>
+        </div>
+    );
 }
