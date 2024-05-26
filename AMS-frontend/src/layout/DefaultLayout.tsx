dac7b80ae060f2a5f4a79abc0b577f8edb236ca0
xagun <xagun.shrestha@gmail.com>
2024-05-26 13:49:50 +0545

    fix: chnage the login page UI

--- AMS-frontend/src/layout/DefaultLayout.tsx
+++ AMS-frontend/src/layout/DefaultLayout.tsx
@@ -3,50 +3,56 @@ import FullWindowSpinner from "@/views/components/FullWindowSpinner";
 import { Link, Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function DefaultLayout() {
-    const { token, fullSpinner } = useStateContext();
-    const location = useLocation();
-    const pathName = location.pathname;
-    if (token) {
-        return <Navigate to="/dashboard" />;
-    }
+  const { token, fullSpinner } = useStateContext();
+  const location = useLocation();
+  const pathName = location.pathname;
+  if (token) {
+    return <Navigate to="/dashboard" />;
+  }
 
-    return (
-        <div className="flex items-center min-h-screen p-4 bg-gray-100 lg:justify-center font-lato ">
-            {fullSpinner && <FullWindowSpinner />}
+  return (
+    <div
+      className="flex items-center min-h-screen p-4 lg:justify-center font-lato"
+      style={{
+        backgroundImage: "url('bg-img.avif')",
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+      }}
+    >
+      {fullSpinner && <FullWindowSpinner />}
 
-            <div className="flex flex-col overflow-hidden bg-white rounded-md shadow-lg max md:flex-row md:flex-1 lg:max-w-[800px]">
-                <div className="p-4 py-6 text-white bg-blue-500 md:w-80 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly">
-                    <div className="my-3 text-4xl font-bold tracking-wider text-center">
-                        <span>AMS</span>
-                        <div className="text-xs">Artist Management System</div>
-                    </div>
-                    <p className="mt-6 font-normal text-center md:mt-0 text-sm ">
-                        Login or sign up now to experience the future of artist
-                        management. With our platform, you can not only view
-                        artists and their music but also take control of your
-                        music universe.
-                    </p>
-                    <p className="flex flex-col items-center justify-center mt-10 text-center">
-                        <span>
-                            {pathName.match("login")
-                                ? "Don't have an account?"
-                                : "Already have an account"}
-                        </span>
-                        {pathName.match("login") ? (
-                            <Link to="/register" className="font-bold">
-                                Register Here
-                            </Link>
-                        ) : (
-                            <Link to="/login" className="font-bold">
-                                Login Here
-                            </Link>
-                        )}
-                    </p>
-                </div>
-                <div className="p-5 bg-white md:flex-1 items-center flex justify-center">
-                    <Outlet />
-                </div>
-            </div>
+      <div className="flex flex-col overflow-hidden rounded-[30px] shadow-lg max md:flex-row md:flex-1 lg:max-w-[1024px]">
+        <div className="p-4 py-6 bg-black bg-opacity-80 text-white md:w-96 md:flex-shrink-0 md:flex md:flex-col md:items-center md:justify-evenly">
+          <div className="my-3 text-4xl font-bold tracking-wider text-center">
+            <span>AMS</span>
+            <div className="text-xs">Artist Management System</div>
+          </div>
+          <p className="mt-6 font-normal text-center md:mt-0 text-sm ">
+            Login or sign up now to experience the future of artist management.
+            With our platform, you can not only view artists and their music but
+            also take control of your music universe.
+          </p>
+          <p className="flex flex-col items-center justify-center mt-10 text-center">
+            <span>
+              {pathName.match("login")
+                ? "Don't have an account?"
+                : "Already have an account"}
+            </span>
+            {pathName.match("register" || "") ? (
+              <Link to="/login" className="font-bold">
+                Login Here
+              </Link>
+            ) : (
+              <Link to="/register" className="font-bold">
+                Register Here
+              </Link>
+            )}
+          </p>
         </div>
-    );
+        <div className="p-5 bg-white md:flex-1 items-center flex justify-center">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
