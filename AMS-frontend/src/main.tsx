61430a64ab45cc69acac3bdeb3e53c4293144a34
xagun <xagun.shrestha@gmail.com>
2024-05-26 13:58:56 +0545

    feat: add shadcn toaster

--- AMS-frontend/src/main.tsx
+++ AMS-frontend/src/main.tsx
@@ -4,11 +4,13 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 import { ContextProvider } from "./context/ContextProvider.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <ContextProvider>
             <RouterProvider router={router} />
+            <Toaster />
         </ContextProvider>
     </React.StrictMode>
 );
