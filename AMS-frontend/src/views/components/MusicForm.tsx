7d2be6a648be6af7c239e79d33b2bbdb146d05ac
xagun <xagun.shrestha@gmail.com>
2024-05-27 10:30:19 +0545

    fix: responsive fixes with some missing validation asterik add

--- AMS-frontend/src/views/components/MusicForm.tsx
+++ AMS-frontend/src/views/components/MusicForm.tsx
@@ -6,186 +6,180 @@ import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 type IProps = {
-    updateAction: boolean;
-    activeArtistId: string;
-    handleMusicDialog: () => void;
-    musicUpdateReq?: any;
+  updateAction: boolean;
+  activeArtistId: string;
+  handleMusicDialog: () => void;
+  musicUpdateReq?: any;
 };
 const MusicForm = ({
-    updateAction,
-    musicUpdateReq,
-    activeArtistId,
-    handleMusicDialog,
+  updateAction,
+  musicUpdateReq,
+  activeArtistId,
+  handleMusicDialog,
 }: IProps) => {
-    const [albumName, setAlbumName] = useState<string>("");
-    const [title, setTitle] = useState<string>("");
-    const [genre, setGenre] = useState<string>("");
-    const [error, setError] = useState<boolean>(false);
-    const [errorObj, setErrorObj] = useState<any>({});
+  const [albumName, setAlbumName] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [genre, setGenre] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [errorObj, setErrorObj] = useState<any>({});
 
-    const { setFullSpinner } = useStateContext();
+  const { setFullSpinner } = useStateContext();
 
-    const handleAddSong = async () => {
-        setFullSpinner(true);
+  const handleAddSong = async () => {
+    setFullSpinner(true);
 
-        const params = {
-            artist_id: activeArtistId,
-            album_name: albumName,
-            genre: genre,
-            title: title,
-        };
-        try {
-            const res = await axiosInstance.post("/music", params);
-            if (res.status === 200 && res.data.success === true) {
-                toast.success(res.data.message);
-                handleMusicDialog();
-                setFullSpinner(false);
-            }
-        } catch (err: any) {
-            // if (err.response.status !== 422) {
-            //     toast.error(err.response.data.message);
-            // }
-            setErrorObj(err.response.data.errors);
-            setError(true);
-            setFullSpinner(false);
-        }
+    const params = {
+      artist_id: activeArtistId,
+      album_name: albumName,
+      genre: genre,
+      title: title,
     };
+    try {
+      const res = await axiosInstance.post("/music", params);
+      if (res.status === 200 && res.data.success === true) {
+        toast.success(res.data.message);
+        handleMusicDialog();
+        setFullSpinner(false);
+      }
+    } catch (err: any) {
+      // if (err.response.status !== 422) {
+      //     toast.error(err.response.data.message);
+      // }
+      setErrorObj(err.response.data.errors);
+      setError(true);
+      setFullSpinner(false);
+    }
+  };
 
-    useEffect(() => {
-        if (musicUpdateReq) {
-            setTitle(musicUpdateReq.title);
-            setAlbumName(musicUpdateReq.album_name);
-            setGenre(musicUpdateReq.genre);
-        }
-    }, [updateAction === true]);
+  useEffect(() => {
+    if (musicUpdateReq) {
+      setTitle(musicUpdateReq.title);
+      setAlbumName(musicUpdateReq.album_name);
+      setGenre(musicUpdateReq.genre);
+    }
+  }, [updateAction === true]);
 
-    const handleUpdateMusic = async () => {
-        setFullSpinner(true);
+  const handleUpdateMusic = async () => {
+    setFullSpinner(true);
 
-        const params = {
-            artist_id: activeArtistId,
-            album_name: albumName,
-            genre: genre,
-            title: title,
-        };
-        try {
-            const res = await axiosInstance.post(
-                `/music/update/${musicUpdateReq.id}`,
-                params
-            );
-            if (res.status === 200 && res.data.success === true) {
-                toast.success(res.data.message);
-                handleMusicDialog();
-                setFullSpinner(false);
-            }
-        } catch (err: any) {
-            // if (err.status !== 422) {
-            //     toast.error(err.response.data.message);
-            // }
-            setErrorObj(err.response.data.errors);
-            setError(true);
-            setFullSpinner(false);
-        }
+    const params = {
+      artist_id: activeArtistId,
+      album_name: albumName,
+      genre: genre,
+      title: title,
     };
+    try {
+      const res = await axiosInstance.post(
+        `/music/update/${musicUpdateReq.id}`,
+        params,
+      );
+      if (res.status === 200 && res.data.success === true) {
+        toast.success(res.data.message);
+        handleMusicDialog();
+        setFullSpinner(false);
+      }
+    } catch (err: any) {
+      // if (err.status !== 422) {
+      //     toast.error(err.response.data.message);
+      // }
+      setErrorObj(err.response.data.errors);
+      setError(true);
+      setFullSpinner(false);
+    }
+  };
 
-    return (
-        <div className="w-full">
-            <div className="flex flex-col space-y-3">
-                <div className="flex gap-4 max-sm:flex-col">
-                    <div className="flex flex-col space-y-1 w-full">
-                        <label
-                            htmlFor="title"
-                            className="text-sm font-semibold text-gray-500"
-                        >
-                            Title <span className="text-red-500"> *</span>
-                        </label>
-                        <input
-                            placeholder="Enter music title"
-                            type="text"
-                            id="title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className={cn(
-                                "inputClass",
-                                error && title === "" && "inputErrorClass"
-                            )}
-                            autoFocus
-                        />
-                        <span className="text-red-500 text-[10px]">
-                            {errorObj?.title}
-                        </span>
-                    </div>
-                </div>
-                <div className="flex gap-4 max-sm:flex-col">
-                    <div className="flex flex-col space-y-1 w-full">
-                        <label
-                            htmlFor="name"
-                            className="text-sm font-semibold text-gray-500"
-                        >
-                            Album name <span className="text-red-500"> *</span>
-                        </label>
-                        <input
-                            placeholder="Enter album name"
-                            type="text"
-                            id="albumName"
-                            value={albumName}
-                            onChange={(e) => setAlbumName(e.target.value)}
-                            className={cn(
-                                "inputClass",
-                                error && albumName === "" && "inputErrorClass"
-                            )}
-                        />
-                        <span className="text-red-500 text-[10px]">
-                            {errorObj?.album_name}
-                        </span>
-                    </div>
-                </div>
-
-                <div className="flex gap-4 max-sm:flex-col">
-                    <div className="flex flex-col space-y-1 w-full">
-                        <label
-                            htmlFor="genre"
-                            className="text-sm font-semibold text-gray-500"
-                        >
-                            Genre <span className="text-red-500"> *</span>
-                        </label>
-                        <select
-                            id="genre"
-                            value={genre}
-                            onChange={(e) => setGenre(e.target.value)}
-                            className={cn(
-                                "inputClass",
-                                error && genre === "" && "inputErrorClass"
-                            )}
-                        >
-                            <option value="" selected>
-                                Select genre
-                            </option>
-                            <option value="rnb">RnB</option>
-                            <option value="classic">Classic</option>
-                            <option value="country">Country</option>
-                            <option value="rock">Rock</option>
-                            <option value="jazz">Jazz</option>
-                        </select>
-                        <span className="text-red-500 text-[10px]">
-                            {errorObj?.genre}
-                        </span>
-                    </div>
-                </div>
-
-                <div className="text-end">
-                    <Button
-                        className="w-[140px] px-4 py-2"
-                        onClick={
-                            updateAction ? handleUpdateMusic : handleAddSong
-                        }
-                    >
-                        {updateAction ? "Update" : "Add Music"}
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div className="w-full">
+      <div className="flex flex-col space-y-3">
+        <div className="flex gap-4 max-sm:flex-col">
+          <div className="flex flex-col space-y-1 w-full text-start">
+            <label
+              htmlFor="title"
+              className="text-sm font-semibold text-gray-500"
+            >
+              Title <span className="text-red-500"> *</span>
+            </label>
+            <input
+              placeholder="Enter music title"
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className={cn(
+                "inputClass",
+                error && title === "" && "inputErrorClass",
+              )}
+              autoFocus
+            />
+            <span className="text-red-500 text-[10px]">{errorObj?.title}</span>
+          </div>
         </div>
-    );
+        <div className="flex gap-4 max-sm:flex-col">
+          <div className="flex flex-col space-y-1 w-full text-start">
+            <label
+              htmlFor="name"
+              className="text-sm font-semibold text-gray-500"
+            >
+              Album name <span className="text-red-500"> *</span>
+            </label>
+            <input
+              placeholder="Enter album name"
+              type="text"
+              id="albumName"
+              value={albumName}
+              onChange={(e) => setAlbumName(e.target.value)}
+              className={cn(
+                "inputClass",
+                error && albumName === "" && "inputErrorClass",
+              )}
+            />
+            <span className="text-red-500 text-[10px]">
+              {errorObj?.album_name}
+            </span>
+          </div>
+        </div>
+
+        <div className="flex gap-4 max-sm:flex-col">
+          <div className="flex flex-col space-y-1 w-full text-start">
+            <label
+              htmlFor="genre"
+              className="text-sm font-semibold text-gray-500"
+            >
+              Genre <span className="text-red-500"> *</span>
+            </label>
+            <select
+              id="genre"
+              value={genre}
+              onChange={(e) => setGenre(e.target.value)}
+              className={cn(
+                "inputClass",
+                error && genre === "" && "inputErrorClass",
+              )}
+            >
+              <option value="" selected>
+                Select genre
+              </option>
+              <option value="rnb">RnB</option>
+              <option value="classic">Classic</option>
+              <option value="country">Country</option>
+              <option value="rock">Rock</option>
+              <option value="jazz">Jazz</option>
+            </select>
+            <span className="text-red-500 text-[10px]">{errorObj?.genre}</span>
+          </div>
+        </div>
+
+        <div className="text-end">
+          <Button
+            className="w-[140px] px-4 py-2"
+            onClick={updateAction ? handleUpdateMusic : handleAddSong}
+          >
+            {updateAction ? "Update" : "Add Music"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MusicForm;
